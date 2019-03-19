52747e33565aa6bf3dcad42eaa9d5be660dc775f
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-03-19 00:26:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var height = "74"
 
 if (name === "Susan"){
   console.log("the name is correct")
+  }
+  else {
+    console.log("The name si not correct")
+  }
   
-}
 
 
 // Don't worry about this
