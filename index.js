c0fed065576076650ebe7f56eee6c5c957ec3aa3
Jeremy Colon Arroyo <jeremy.seth19@gmail.com>
2019-03-19 00:23:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Susan"){
+  console.log("the name is correct");
+  else if 
+  console.log("the name is incorrect");
+}
+
 
 // Don't worry about this
 module.exports = { name, height
 }
-
